6b4a4a694d36dfb1135e935d197cdf92ba0cf02d
michalflis <michalt.flis@gmail.com>
2021-07-22 19:53:22 +0200

    Corrected behaviour for null argument

--- src/main/java/CapitalLetters.java
+++ src/main/java/CapitalLetters.java
@@ -1,6 +1,8 @@
 public class CapitalLetters {
 
     public String convertToCapitalLetters (String givenText) {
+        if (givenText == null)
+            return "";
         return givenText.toUpperCase();
 
     }
