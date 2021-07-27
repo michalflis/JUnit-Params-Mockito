20a41c943590c23d3735c2505ca94d4e75a0c169
michalflis <michalt.flis@gmail.com>
2021-07-27 22:01:23 +0200

    Created Names class

---
+++ src/main/java/javacollections/NamesMain.java
@@ -0,0 +1,12 @@
+package javacollections;
+
+public class NamesMain {
+
+    public static void main(String[] args){
+        Names names = new Names();
+
+        names.getListOfNamesWithoutDuplicates();
+        names.getReversedListOfNames();
+        names.replaceName("Anna", "Joanna");
+    }
+}
