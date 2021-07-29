7ef65071d0248a4664c2cf82473d45df2508ad76
michalflis <michalt.flis@gmail.com>
2021-07-29 19:54:30 +0200

    Added tests

---
+++ src/test/java/javacollections/MyOwnListTest.java
@@ -0,0 +1,52 @@
+package javacollections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class MyOwnListTest {
+
+    @Test
+    void shouldReturnSizeOfList_1_2_3() {
+        //given
+        int[] array = {1, 2, 3};
+        MyOwnList myOwnList = new MyOwnList(array);
+
+        //when
+        int result = myOwnList.size();
+
+        //then
+        Assertions.assertEquals(result, 3);
+    }
+
+    @Test
+    void ShouldCheckIfListIsEmpty_1_2_3() {
+        //given
+        int[] array = {1, 2, 3};
+        MyOwnList myOwnList = new MyOwnList(array);
+
+        //when
+        boolean result = myOwnList.isEmpty();
+
+        //then
+        Assertions.assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void shouldGetValueFromIndexOfList(int input) {
+        //given
+        int[] array = {1, 2, 3};
+        MyOwnList myOwnList = new MyOwnList(array);
+
+        //when
+        int result = myOwnList.get(input);
+
+        //then
+        Assertions.assertEquals(result, array[input]);
+
+    }
+
+}
