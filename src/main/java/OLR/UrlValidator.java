0640ea3a2a87d0ff4e7abe63cf517a6346194c4f
michalflis <michalt.flis@gmail.com>
2021-08-03 19:24:43 +0200

    Created URL validator

---
+++ src/main/java/OLR/UrlValidator.java
@@ -0,0 +1,27 @@
+package OLR;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UrlValidator {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wprowadź adres URL");
+        String pesel = scan.nextLine();
+        if (peselValidate(pesel)) System.out.println("Prawidłowy adres URL");
+        else System.out.println("Błędny adres URL");
+
+    }
+
+    public static boolean peselValidate(String pesel) {
+
+        Pattern pattern = Pattern.compile("https?://[0-9A-z-_~]*.[0-9A-z-_~]*.[0-9A-z-_~]*");
+        Matcher matcher = pattern.matcher(pesel);
+        return matcher.matches();
+
+    }
+
+}
